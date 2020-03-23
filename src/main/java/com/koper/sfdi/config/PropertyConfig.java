fdf7315d0de89d86441f5efd13c4e1f339959fe6
idan koper <idankoper@gmail.com>
2020-03-23 13:50:08 +0200

    Using environment properties (Override the properties file).

--- src/main/java/com/koper/sfdi/config/PropertyConfig.java
+++ src/main/java/com/koper/sfdi/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package com.koper.sfdi.config;
 
 import com.koper.sfdi.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${koper.username}")
     String user;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("username"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
